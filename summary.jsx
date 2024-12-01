9820f96a7c20da7daaa5a0558d9b44f3646d5794
Lucas <lhermiz@oakland.edu>
2024-12-01 06:57:33 -0500

    Create summary.jsx

---
+++ summary.jsx
@@ -0,0 +1,17 @@
+"use client";
+const Summary = () => {
+  return (
+    <div className="summary-page">
+      <h2>Summary</h2>
+      <p>
+        Driven senior with a deep passion for cybersecurity, pursuing Google
+        Security Plus and CompTIA Security+ certifications. Proficient in
+        network security, cybersecurity principles, and tools like Wireshark,
+        Linux, Nmap, and GitHub. Excels in team-based projects and independent
+        roles, available full-time as of April.
+      </p>
+    </div>
+  );
+};
+
+export default Summary;

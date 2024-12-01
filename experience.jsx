51c94a04bca3ea9bf1967055f61e534722ae20b1
Lucas <lhermiz@oakland.edu>
2024-12-01 06:57:55 -0500

    Create experience.jsx

---
+++ experience.jsx
@@ -0,0 +1,17 @@
+"use client";
+const Experience = () => {
+  return (
+    <div className="experience-page">
+      <h2>Experience</h2>
+      <h3>Pro Care Unlimited | Billing Specialist</h3>
+      <p>Southfield, MI | 02/2021- Present</p>
+      <ul>
+        <li>Collaborated with a team of 6 to efficiently verify and process timesheets weekly, ensuring accurate billing and timely submissions.</li>
+        <li>Developed a Python script to extract data from Excel sheets, automating repetitive tasks with high speed and precision.</li>
+        <li>Increased productivity by 75% and reduced operational costs by 20% through improved workflows.</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Experience;

344caf07200f640cd87413ff9938377514ebd334
Lucas <lhermiz@oakland.edu>
2024-12-01 06:53:44 -0500

    Create Projects.jsx

---
+++ Projects.jsx
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div className="projects-section">
+      <h2>Projects</h2>
+      <ul>
+        <li>
+          <strong>Network-Wide Ad Blocker:</strong> Using a Raspberry Pi to
+          filter unwanted advertisements.
+        </li>
+        <li>
+          <strong>Robotics Project:</strong> Built a robot with sensors and
+          remote-controlled movement.
+        </li>
+        <li>
+          <strong>Automated Buying Tool:</strong> Automated tasks across
+          accounts for product purchases.
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Projects;

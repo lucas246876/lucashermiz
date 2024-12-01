d57d69376d6b769d0a7dc3647392814c30d36cd0
Lucas <lhermiz@oakland.edu>
2024-12-01 06:53:14 -0500

    Create Intro.jsx

---
+++ Intro.jsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div className="intro-section">
+      <h1>Hi, I'm Lucas Hermiz</h1>
+      <p>
+        A passionate cybersecurity enthusiast pursuing certifications like
+        Google Security Plus and CompTIA Security+. Skilled in tools like
+        Wireshark, Linux, Nmap, and GitHub.
+      </p>
+    </div>
+  );
+};
+
+export default Intro;

6e5d3b3952a292a4b165641519fbac4ea682a7ce
Lucas <lhermiz@oakland.edu>
2024-12-01 06:58:14 -0500

    Create projects.jsx

---
+++ projects.jsx
@@ -0,0 +1,15 @@
+"use client";
+const Projects = () => {
+  return (
+    <div className="projects-page">
+      <h2>Projects</h2>
+      <ul>
+        <li><strong>Network-Wide Ad Blocker:</strong> Using a Raspberry Pi to filter unwanted advertisements.</li>
+        <li><strong>Robotics Project:</strong> Built a robot with sensors and remote-controlled movement.</li>
+        <li><strong>Automated Buying Tool:</strong> Automated tasks across accounts for product purchases.</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Projects;
